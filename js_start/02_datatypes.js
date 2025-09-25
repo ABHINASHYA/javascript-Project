47dc76d88d056b7093ccdd352fc5c422f9ece939
ABHINASHYA <abhinashkumarshb7@gmail.com>
2025-09-26 01:33:12 +0530

    Updated variables

--- js_start/02_datatypes.js
+++ js_start/02_datatypes.js
@@ -16,4 +16,66 @@ let country = null; //null
 //symbol => unique identifiers
 
 
-console.log(typeof undefined);
+//console.log(typeof undefined);
+
+//data types are two types
+//1. Primitive data types
+//2. Reference data types
+
+
+//Primitive data types = 7
+// number, string, boolean, null, undefined, symbol, bigint
+
+const score = 100; // number
+const scoreValue = 100.5; // number
+
+// const isLoggedIn = false; //boolean
+const outsideTemp = null;
+let userEmail; //undefined
+
+const id = Symbol('258');
+const anotherId = Symbol('258');
+
+console.log(id === anotherId); // false
+
+const bigNumber = 3456789012345678901234567890n;
+
+//Reference(non-primitive) data types = 1
+// object
+// Array
+// function
+
+//object
+let person = {
+	name: "Abhinash",
+	age: 21,
+	isLoggedIn: false
+};
+//console.log(typeof person);
+//console.log(person);
+
+//Array
+let heroes = ["Shaktiman", "Naagraj", "Doga", 45, null, undefined, false];
+let myObj = {
+	name: "Abhinash",
+	age: 21,
+};
+const myfunction = function () {
+	console.log("Hello World");
+}
+
+console.log(typeof myfunction);
+
+
+// types of value and result
+
+//undefined => undefined
+//null => object
+//boolean => true/false
+//number => number
+//string => string
+//bigint => bigint
+//symbol => symbol
+//object => object
+//array => object
+//function => function
---
+++ js_start/04_comparisions.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(4 < 8);
+// console.log(2 == 3);
+// console.log(2 != 3);
+
+
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+// console.log("01" > "1"); // false
+
+/*
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+
+// This type of comparision creates confusion so avoid using null in comparision
+
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0);
+*/
