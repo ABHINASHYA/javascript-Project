bfbe7b5bc72a17326904cce4dc22e2b1393d4360
ABHINASHYA <abhinashkumarshb7@gmail.com>
2025-09-25 17:07:21 +0530

    first commit

---
+++ js_start/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 3948;
+let accountEmail = "abhinash@example.com";
+var accountPassword = "12345";
+accountCity = "Bhilai";
+let accountState;
+
+//accountId = 6;/// not allowed
+
+accountEmail = "abhinash123@example.com";
+accountPassword = "8475983";
+accountCity = "Patna"
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+
+
---
+++ js_start/02_datatypes.js
@@ -0,0 +1,19 @@
+let name = "Abhinash" //string
+let age = 21; //number
+let isLoggedIn = false; //boolean
+let state; //undefined
+let country = null; //null
+
+
+
+//Number => 2 to the power of 53
+//bigInt => larger than 2 to the power of 53
+//String => ""   sequence of characters
+//Boolean => true/false
+//null => standalone value
+//undefined => variable not assigned a value
+//object => key value pairs
+//symbol => unique identifiers
+
+
+console.log(typeof undefined);
